23666b4c9cf795562fedf4d0a84bbf4b4e04d420
MohammedAdnan <MohammedAdnan.cs@hotmail.com>
2024-04-24 21:28:47 +0200

    animations to homepage

--- frontend/src/Components/OurPartners/ourpartners.jsx
+++ frontend/src/Components/OurPartners/ourpartners.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './ourpartners.css'
 import bmw from '../Assets/bmw.png'
 import ferrari from '../Assets/ferrari.png'
@@ -8,14 +8,34 @@ import porsche from '../Assets/porsche.png'
 import renault from '../Assets/renault.png'
 import peugeot from '../Assets/peugeot.png'
 import Toyota from '../Assets/Toyota.png'
+import { motion, useInView } from 'framer-motion'
+
 
 const Hero = () => {
 
     const Partnerslogos = [bmw, ferrari, mercedes, nissan, porsche, renault, peugeot, Toyota]
+    const textP = "Our Partners"
+
+    const defaultAnimations = {
+        hidden: {
+            opacity: 0,
+            y: 20,
+        },
+        visible: {
+            opacity: 1,
+            y: 0,
+        },
+    }
+    const ref = useRef(null);
+    const isInView = useInView(ref, { amount: 0.5 });
 
     return (
         <div className='ourpartners'>
-            <p>Our Partners</p>
+
+            <motion.p ref={ref} transition={{ staggerChildren: 0.1 }} initial="hidden" animate={isInView ? "visible" : "hidden"} aria-hidden align="justify">{textP.split("").map((char) => (
+                <motion.span style={{ display: "inline-block" }} variants={defaultAnimations}>{char}</motion.span>
+            ))}</motion.p>
+
             <div className='Wrapper'>
                 <div className='Marquee'>
                     <div className='Marqueegroup'>
