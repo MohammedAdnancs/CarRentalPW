1fdeab732f0bbe9288c5a5e7aaff65755e6b05e4
MohammedAdnan <MohammedAdnan.cs@hotmail.com>
2024-05-08 00:33:05 +0300

    css

--- backend/models/Listing.js
+++ backend/models/Listing.js
@@ -5,12 +5,13 @@ const listingSchema = new Schema({
     carName: String,
     carType: String,
     numDoors: Number,
-    numSeats: Number, 
-    price: Number, 
+    numSeats: Number,
+    price: Number,
     location: String,
     description: String,
     image1: String,
-    image2: String
+    image2: String,
+    ownerId: String
 });
 
 const ListingModel = mongoose.model('Listing', listingSchema);
