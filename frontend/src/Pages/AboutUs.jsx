f955c00c6543267da4df209a4c756448c79c5380
AndrewHabel <andrewmagdy545@gmail.com>
2024-05-14 20:17:51 +0300

    assem is dumb

--- frontend/src/Pages/AboutUs.jsx
+++ frontend/src/Pages/AboutUs.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
+import AboutBox from '../Components/AboutBox/AboutBox'
 
 const AboutUs = () => {
     return (
         <div className='AboutUs'>
-
+            <AboutBox />
         </div>
     )
 }
---
+++ frontend/src/Pages/ListingInfo.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import ListingDetails from '../Components/ListingDetails/ListingDetails'
+
+const ListingInfo = () => {
+    return (
+        <div className='ListingInfo'>
+            <ListingDetails />
+        </div>
+    )
+}
+
+export default ListingInfo
